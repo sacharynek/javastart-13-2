636bd036b06faf4bf5a0a55c3cb39cd994f5c007
tomaszlach <tomaszlach86@gmail.com>
2019-06-12 13:18:02 +0200

    added few cases

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         TabCalculator tc = new TabCalculator();
         System.out.println(Arrays.toString(tc.processTab(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 9, 9}, 3)));
+        System.out.println(Arrays.toString(tc.processTab(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 9, 9}, 5)));
+        System.out.println(Arrays.toString(tc.processTab(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9, 9, 9}, 20)));
+
     }
 
 }
