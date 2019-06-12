4dab1b7e527e34158b2687c83378ff99399c1025
tomaszlach <tomaszlach86@gmail.com>
2019-06-12 13:16:46 +0200

    improved styling

--- src/main/java/TabCalculator.java
+++ src/main/java/TabCalculator.java
@@ -1,16 +1,15 @@
-public class TabCalculator {
+class TabCalculator {
 
-    public int[] processTab(int[] tab, int N) {
+    int[] processTab(int[] tab, int N) {
         if (N > tab.length / 2) {
-            throw new IllegalArgumentException("Argument N jest za duży!");
+            throw new ArrayIndexOutOfBoundsException("Argument N jest za duży!");
         }
-
         int[][] tabOfTabs = this.divideTabs(tab, N);
 
         return sumTabs(tabOfTabs);
     }
 
-    public int[][] divideTabs(int[] tab, int N) {
+    private int[][] divideTabs(int[] tab, int N) {
         double tablen = (double) tab.length;
         int lengthOfSubtab = (int) Math.ceil((tablen / N));
 
@@ -28,9 +27,8 @@ public class TabCalculator {
         return output;
     }
 
-    public int[] sumTabs(int[][] tabOfTabs) {
+    private int[] sumTabs(int[][] tabOfTabs) {
         int[] output = new int[tabOfTabs.length];
-        int sum = 0;
 
         for (int i = 0; i < tabOfTabs.length; i++) {
             for (int j = 0; j < tabOfTabs[i].length; j++) {
